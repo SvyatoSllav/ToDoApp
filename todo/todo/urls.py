cf77737b31d1a12a76fef57217473324eec28545
Александр <tarasov.slavas2002@gmail.com>
2022-04-29 18:09:43 +0000

    added swagger and requirements

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -13,10 +13,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('', include('tasks.urls', namespace='tasks')),
     path('admin/', admin.site.urls),
+]
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ToDo API",
+      default_version='v1',
+      description="Документация для приложения api проекта ToDo",
+      contact=openapi.Contact(email="tarasov.slavas2002@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+   url(r'^swagger(?P<format>\.json|\.yaml)$',
+       schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+       name='schema-swagger-ui'),
+   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+       name='schema-redoc'),
 ]
