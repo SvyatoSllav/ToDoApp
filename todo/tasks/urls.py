a2aae3176f963fa3d8c24ad1a2e221fd4bb6b549
Александр <tarasov.slavas2002@gmail.com>
2022-05-05 13:59:13 +0000

    added complete url

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -7,6 +7,7 @@ app_name = 'tasks'
 
 urlpatterns = [
     path('', views.index, name='list'),
+    path('complete/<str:pk>/', views.complete_task, name='complete_task'),
     path('update_task/<str:pk>/', views.update_task, name='update_task'),
     path('delete/<str:pk>/', views.delete_task, name='delete_task'),
     path('api/', include('api.urls'))
