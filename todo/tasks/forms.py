0bbb49c4b4845476196aa9b9dc5f523a840f9bf0
Александр <tarasov.slavas2002@gmail.com>
2022-05-05 13:16:49 +0000

    added form for edit task page

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -4,7 +4,23 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Введите текст'})
+    )
 
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ('title', )
+
+
+class UpdateTaskForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Введите текст'})
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(),
+    )
+
+    class Meta:
+        model = Task
+        fields = ('title', 'description')
