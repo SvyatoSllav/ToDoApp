3502030b906c65fdadff061cb9f5b4e2c32997a1
Александр <tarasov.slavas2002@gmail.com>
2022-05-05 13:17:04 +0000

    added description field

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='Название задачи')
+    description = models.TextField(blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
