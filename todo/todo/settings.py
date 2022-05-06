70a25d7a5b22682ff2c03bdb1c698e06b52559e5
Александр <tarasov.slavas2002@gmail.com>
2022-05-06 19:27:29 +0000

    added db update config

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
