283af67ad890452158bbde3c7879d98335b71e3b
Александр <tarasov.slavas2002@gmail.com>
2022-05-06 18:51:06 +0000

    added unrequired field description in serializer

--- todo/api/serializer.py
+++ todo/api/serializer.py
@@ -4,7 +4,8 @@ from tasks.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    description = serializers.CharField(required=False)
 
     class Meta:
         model = Task
-        fields = ('id', 'title', 'complete', 'created')
+        fields = ('id', 'title', 'complete', 'created', 'description')
